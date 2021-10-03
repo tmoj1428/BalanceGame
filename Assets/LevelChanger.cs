8320b6866c92cd907ea450a8c50c546e76a5e392
mohammad javad taheri <tmoj.1428@gmail.com>
2021-10-03 11:41:58 +0330

    Add level change fade animation to Intro

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+     
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/Wait.cs
+++ Assets/Scripts/Wait.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Wait : MonoBehaviour
 {
+    public LevelChanger change;
+
     public float waitTime = 5f;
     void Start()
     {
@@ -14,8 +16,7 @@ public class Wait : MonoBehaviour
     IEnumerator Wait_for_intro()
     {
         yield return new WaitForSeconds(waitTime);
-
-        SceneManager.LoadScene(1);
+        change.FadeToLevel(1);
     }
 
 }
