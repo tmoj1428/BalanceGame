722f0217d03ddd15962b5a5217ab81c1a7f73dbf
mohammad javad taheri <tmoj.1428@gmail.com>
2021-10-03 10:37:53 +0330

    Add an intro to the game

---
+++ Assets/Scripts/IntroText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroText : MonoBehaviour
+{
+    private Text introText;
+    private float timer;
+    private int characterIndex;
+    private float timerperCharacter = .1f;
+    private string textToWrite;
+
+    private void Awake()
+    {
+        introText = transform.Find("IntroText").GetComponent<Text>();
+    }
+    void Start()
+    {
+        textToWrite = "Balance";
+        characterIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (introText != null)
+        {
+            timer -= Time.deltaTime;
+            while (timer <= 0f)
+            {
+                timer += timerperCharacter;
+                characterIndex++;
+                introText.text = textToWrite.Substring(0, characterIndex);
+
+                if (characterIndex >= textToWrite.Length)
+                {
+                    introText = null;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    public float waitTime = 5f;
+    void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(1);
+    }
+
+}
